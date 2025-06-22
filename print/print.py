2cd048bae56d5db49e2abf04deb62c1fec43339b
cja <1106405773@qq.com>
2025-06-22 20:46:48 +0800

    修复了wps调用打印机存在的问题

--- print/print.py
+++ print/print.py
@@ -2,22 +2,10 @@ import win32com.client
 import win32print
 
 
-
-def get_pdf_printer_port():
-    printers = win32print.EnumPrinters(2)
-    for flags, desc, name, comment in printers:
-        if name == "Microsoft Print to PDF":
-            printer_info = win32print.GetPrinter(win32print.OpenPrinter(name), 2)
-            port = printer_info["pPortName"]
-            return f"{name} on {port}"
-    return None
-
-
 wpsCom = win32com.client.Dispatch('Ket.Application')
 wpsCom.Visible = False
 wpsCom.DisplayAlerts = False
 wpsCom.PrintCommunication = False
-wpsCom.ActivePrinter = get_pdf_printer_port()
 def printExcel(excel_file):
     try:
         workbook = wpsCom.Workbooks.Open(excel_file)
